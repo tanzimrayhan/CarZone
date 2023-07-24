2cd0f0f1c472c53307909d7d8c73ab686719911b
Tanzimrayhan <Tanzim_rayhan@yahoo.com>
2023-07-24 14:07:50 -0400

    fixed build errors

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { fuels, yearsOfProduction } from '@/constants';
 import { fetchCars } from '@/utils'
 import Image from 'next/image'
 
-export default async function Home({searchParams}) {
+export default async function Home({searchParams}:any) {
   const allCars=await fetchCars({
     manufecturer : searchParams.manufecturer ||'',
     year : searchParams.year ||2022,
